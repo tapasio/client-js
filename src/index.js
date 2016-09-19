ed09cd38db0a203261f33460925dc07dfbb0b09f
Anthony Sendra <asendra@kaliop.com>
2016-09-19 16:46:51 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ import TapasQuery from './TapasQuery'
 
 function Tapas (config) {
   this.cache = {users: {name: 'tutu'}}
+  this.batch = []
 }
 
 Tapas.prototype.need = function (need) {
-  return new TapasQuery(this.cache, this.cache, need)
+  let tapasQuery = new TapasQuery(this.batch, this.cache, need)
+  this.batch.push(tapasQuery)
+
+  return tapasQuery
+}
+
+Tapas.prototype.execute = function (tapasQueries) {
+
 }
 
 export default Tapas
