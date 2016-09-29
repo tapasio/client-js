fc68f7c45323c5a372110bf98fbfc8b4abf8959f
Luca Marchesini <xbill82@gmail.com>
2016-09-29 22:48:21 +0200

    Added some TDD (empty) tests

---
+++ test/Tapas.spec.js
@@ -0,0 +1,21 @@
+describe('Tapas', () => {
+	describe('constructor', () => {
+    it('should accept transport and url arguments', () => {
+			
+		})
+	})
+
+  describe('need', () => {
+    it('method should be exposed', () => {
+      
+    })
+
+    it('should accept an argument of type Object', () => {
+      
+    })
+
+    it('should return a TapasQuery object', () => {
+      
+    })
+  })
+})
---
+++ test/TapasResponse.spec.js
@@ -0,0 +1,27 @@
+describe('TapasResponse', () => {
+  describe('constructor', () => {
+    it('should extend Rx.Observable', () => {
+      
+    })
+  })
+
+  describe('subscribe', () => {
+    it('method should be exposed', () => {
+      
+    })
+
+    it('should call Rx.Observable.subscribe', () => {
+      
+    })
+  })
+
+  describe('then', () => {
+    it('method should be exposed', () => {
+      
+    })
+
+    it('should call Rx.Observable.subscribe then unsubscribe after the first response', () => {
+      
+    })
+  })
+})
